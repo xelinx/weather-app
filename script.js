8a26f93251ddbcdb5a30b22d755b1b9b220c0007
Mona <66236313+xelinx@users.noreply.github.com>
2020-07-22 23:38:19 -0700

    Move javascript to separate file, update divs

---
+++ script.js
@@ -0,0 +1,41 @@
+    //Variable for city
+    var cityName = "";
+    
+    // This is our API key
+    var APIKey = "4329b9a304464e3aaf6df7df53ecd8b3";
+
+    // Here we are building the URL we need to query the database
+    var queryURL = "api.openweathermap.org/data/2.5/forecast?q={" + cityName + "}&appid=" +
+    + APIKey;
+
+    // AJAX call to the OpenWeatherMap API
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // We store all of the retrieved data inside of an object called "response"
+      .then(function(response) {
+
+        // Log the queryURL
+        console.log(queryURL);
+
+        // Log the resulting object
+        console.log(response);
+
+        // Transfer content to HTML
+        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        
+        // Convert the temp to fahrenheit
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+        // add temp content to html
+        $(".temp").text("Temperature (K) " + response.main.temp);
+        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+        // Log the data in the console as well
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + tempF);
+      });
