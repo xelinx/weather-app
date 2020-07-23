3c5d6db98d8fb86dcb4e949dbe65197160c292ad
Mona <66236313+xelinx@users.noreply.github.com>
2020-07-23 01:42:55 -0700

    updated javascript outline

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
     //Variable for city
-    var cityName = "";
+    var cityName = document.getElementById("cityName");
+
+    //Array for history of search
+
     
     // This is our API key
     var APIKey = "4329b9a304464e3aaf6df7df53ecd8b3";
 
     // Here we are building the URL we need to query the database
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?q={" + cityName + "}&appid=" +
+    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&exclude=hourly&units=imperial&appid=" +
     + APIKey;
 
     // AJAX call to the OpenWeatherMap API
@@ -23,19 +26,15 @@
         console.log(response);
 
         // Transfer content to HTML
-        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".city").html(response.name);
+        $(".tempF").html("Temperature:" + response.tempF.toFixed(2));
         $(".humidity").text("Humidity: " + response.main.humidity);
-        
-        // Convert the temp to fahrenheit
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-
-        // add temp content to html
-        $(".temp").text("Temperature (K) " + response.main.temp);
-        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".UV").text("UV Index: " + response.uvi);
 
         // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
         console.log("Temperature (F): " + tempF);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("UV Index: " + response.uvi);
       });
