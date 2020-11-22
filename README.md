4ec661c61fceb17fd258c55af25d6764d407c8ea
Fog <lin.emay@gmail.com>
2020-11-22 01:19:29 -0800

    update script

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ To design a weather Dashboard that displays the current weather and 5-day foreca
 
 I had a lot of trouble with the weather app. Setting up HTML was relatively easy. Setting up search history was also ok. However, I had trouble appending my weather data to the page. I keep getting a CORS policy error, and I can't figure out how to fix it. The query URLs are correct, and going to the URL shows me the information I need.
 
+Edit: I have fixed the API call problem and cleared up the script.
+
 ## Screenshots of the Deployed Website
 ![Screen Shot 2020-07-25 at 3 22 45 AM](https://user-images.githubusercontent.com/66236313/88454703-2ef35700-ce26-11ea-9e66-4159e7a5daf0.png)
 
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Variable for city
-var cityName = $("cityName").val();
+//var cityName = $("cityName").val();
 
 //Array for history of search
 var cities = [];
@@ -7,6 +7,7 @@ var cities = [];
 // This is our API key, latitude, and longitude
 var APIKey = "4329b9a304464e3aaf6df7df53ecd8b3";
 
+getWeather();
 //Current Weather forecast
 function getWeather(cityName) {
 
@@ -45,6 +46,31 @@ function getWeather(cityName) {
       $(".humidity").text("Humidity: " + humidity + "%");
       $(".wind").text("Wind Speed: " + wind + "mph");
 
+      //Get 5-Day Forecast
+      const forecast = document.querySelectorAll(".forecast");
+      for (i = 0; i < forecast.length; i++) {
+        forecast[i].innerHTML = "";
+        const forecastIndex = i * 8 + 4;
+        const forecastDate = new Date(response.list[forecastIndex].dt * 1000);
+        const forecastDay = forecastDate.getDate();
+        const forecastMonth = forecastDate.getMonth() + 1;
+        const forecastYear = forecastDate.getFullYear();
+        const forecastDateP = document.createElement("p");
+        forecastDateP.setAttribute("class", "mt-3 mb-0 forecast-date");
+        forecastDateP.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+        forecast[i].append(forecastDateP);
+        const forecastWeather = document.createElement("img");
+        forecastWeather.setAttribute("src", "https://openweathermap.org/img/wn/" + response.list[forecastIndex].weather[0].icon + "@2x.png");
+        forecastWeather.setAttribute("alt", response.list[forecastIndex].weather[0].description);
+        forecast[i].append(forecastWeather);
+        const forecastTemp = document.createElement("p");
+        forecastTemp.innerHTML = "Temp: " + response.list[forecastIndex].main.temp + " ºF";
+        forecast[i].append(forecastTemp);
+        const forecastHumidity = document.createElement("p");
+        forecastHumidity.innerHTML = "Humidity: " + response.list[forecastIndex].main.humidity + "%";
+        forecast[i].append(forecastHumidity);
+      }
+
       // Define lat/lon for AJAX call
       let lat = response.coord.lat;
       let lon = response.coord.lon;
@@ -57,36 +83,11 @@ function getWeather(cityName) {
       })
         // log response
         .then(function (response) {
-          console.log(response);
+          //console.log(response);
 
-          //Get 5-Day Forecast
-          const forecast = document.querySelectorAll(".forecast");
-          for (i = 0; i < forecast.length; i++) {
-            forecast[i].innerHTML = "";
-            const forecastIndex = i * 8 + 4;
-            const forecastDate = new Date(response.daily[forecastIndex].dt * 1000);
-            const forecastDay = forecastDate.getDate();
-            const forecastMonth = forecastDate.getMonth() + 1;
-            const forecastYear = forecastDate.getFullYear();
-            const forecastDateP = document.createElement("p");
-            forecastDateP.setAttribute("class", "mt-3 mb-3 forecast-date");
-            forecastDateP.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
-            forecast[i].append(forecastDateP);
-            const forecastWeather = document.createElement("img");
-            forecastWeather.setAttribute("src", "https://openweathermap.org/img/wn/" + response.daily[forecastIndex].weather[0].icon + "@2x.png");
-            forecastWeather.setAttribute("alt", response.daily[forecastIndex].weather[0].description);
-            forecast[i].append(forecastWeather);
-            const forecastTemp = document.createElement("p");
-            forecastTemp.innerHTML = "Temp: " + response.daily[forecastIndex].temp.day + " ºF";
-            forecast[i].append(forecastTemp);
-            const forecastHumidity = document.createElement("p");
-            forecastHumidity.innerHTML = "Humidity: " + response.daily[forecastIndex].humidity + "%";
-            forecast[i].append(forecastHumidity);
-            
-            //Get UVI
-            var UVI = response.current.uvi
-            $(".UV").text("UVI: " + UVI.toFixed(2));
-          }
+          //Get UVI
+          var UVI = response.current.uvi
+          $(".UV").text("UVI: " + UVI.toFixed(2));
         });
     });
 };
